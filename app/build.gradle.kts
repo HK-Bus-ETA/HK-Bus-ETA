450255dece7ecb079a44b5e0c1f66c7312de3c14
LOOHP <jamesloohp@gmail.com>
2023-09-04 01:46:05 +0100

    Added scrollbars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.loohp.hkbuseta"
         minSdk = 30
         targetSdk = 33
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
         vectorDrawables {
             useSupportLibrary = true
         }
