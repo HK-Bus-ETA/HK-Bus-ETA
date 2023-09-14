77f0e6521a2f8d844c33e334f0ec0e25b30579b0
LOOHP <jamesloohp@gmail.com>
2023-09-14 02:42:38 +0100

    Version bump

--- HKBusETA/app/build.gradle.kts
+++ HKBusETA/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.loohp.hkbuseta"
         minSdk = 30
         targetSdk = 33
-        versionCode = 48
+        versionCode = 49
         versionName = "1.5.1"
         vectorDrawables {
             useSupportLibrary = true
