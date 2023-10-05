d118eda1ec27f97ad23bf2ecd325610a098c2c27
LOOHP <jamesloohp@gmail.com>
2023-10-05 15:09:37 +0100

    Retain line numbers

--- HKBusETA/app/build.gradle.kts
+++ HKBusETA/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.loohp.hkbuseta"
         minSdk = 30
         targetSdk = 33
-        versionCode = 93
+        versionCode = 94
         versionName = "1.5.7"
         vectorDrawables {
             useSupportLibrary = true
