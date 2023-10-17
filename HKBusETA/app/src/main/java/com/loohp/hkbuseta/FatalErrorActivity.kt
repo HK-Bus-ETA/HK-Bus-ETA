b7fe7c25975d8cd1208ed1ee24529d6aae2f9236
LOOHP <jamesloohp@gmail.com>
2023-10-17 01:15:33 +0100

    Addition guidance in the crash screen

--- HKBusETA/app/src/main/java/com/loohp/hkbuseta/FatalErrorActivity.kt
+++ HKBusETA/app/src/main/java/com/loohp/hkbuseta/FatalErrorActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.TweenSpec
-import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.gestures.animateScrollBy
@@ -41,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -65,18 +65,9 @@ class FatalErrorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val zh: String?
-        val en: String?
-        val exception: String?
-        if (intent.extras == null) {
-            zh = null
-            en = null
-            exception = null
-        } else {
-            zh = intent.extras!!.getString("zh")
-            en = intent.extras!!.getString("en")
-            exception = intent.extras!!.getString("exception")
-        }
+        val zh = intent.extras?.getString("zh")
+        val en = intent.extras?.getString("en")
+        val exception = intent.extras?.getString("exception")
 
         Shared.invalidateCache(this)
 
@@ -94,14 +85,6 @@ fun Message(instance: FatalErrorActivity, zh: String?, en: String?, exception: S
         }
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Top
-        ) {
-            Shared.MainTime()
-        }
-        Column(
-            modifier = Modifier
                 .fillMaxSize(),
             verticalArrangement = Arrangement.Center
         ) {
@@ -110,7 +93,8 @@ fun Message(instance: FatalErrorActivity, zh: String?, en: String?, exception: S
                     .fillMaxWidth()
                     .padding(20.dp, 0.dp),
                 textAlign = TextAlign.Center,
-                fontSize = TextUnit(StringUtils.scaledSize(16F, instance), TextUnitType.Sp),
+                fontWeight = FontWeight.Bold,
+                fontSize = StringUtils.scaledSize(if (zh == null) 14F else 16F, instance).dp.sp,
                 color = MaterialTheme.colors.primary,
                 text = zh ?: "發生錯誤"
             )
@@ -119,10 +103,31 @@ fun Message(instance: FatalErrorActivity, zh: String?, en: String?, exception: S
                     .fillMaxWidth()
                     .padding(20.dp, 0.dp),
                 textAlign = TextAlign.Center,
-                fontSize = TextUnit(StringUtils.scaledSize(16F, instance), TextUnitType.Sp),
+                fontWeight = FontWeight.Bold,
+                fontSize = StringUtils.scaledSize(if (en == null) 14F else 16F, instance).dp.sp,
                 color = MaterialTheme.colors.primary,
                 text = en ?: "Fatal Error Occurred"
             )
+            if (zh == null && en == null) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 0.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = StringUtils.scaledSize(10F, instance).dp.sp,
+                    color = MaterialTheme.colors.primary,
+                    text = "巴士路線資料可能不完整\n點選重新載入刷新"
+                )
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp, 0.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = StringUtils.scaledSize(10F, instance).dp.sp,
+                    color = MaterialTheme.colors.primary,
+                    text = "Bus route data might be corrupted\nClick reload to refresh"
+                )
+            }
             if (exception != null) {
                 val focusRequester = remember { FocusRequester() }
                 val scroll = rememberScrollState()
@@ -154,7 +159,7 @@ fun Message(instance: FatalErrorActivity, zh: String?, en: String?, exception: S
                 Text(
                     modifier = Modifier
                         .fillMaxWidth(0.95F)
-                        .fillMaxHeight(0.3F)
+                        .fillMaxHeight(0.25F)
                         .verticalScrollWithScrollbar(
                             state = scroll,
                             flingBehavior = ScrollableDefaults.flingBehavior(),
@@ -177,8 +182,8 @@ fun Message(instance: FatalErrorActivity, zh: String?, en: String?, exception: S
                         .focusable()
                         .padding(20.dp, 0.dp),
                     textAlign = TextAlign.Left,
-                    fontSize = TextUnit(10F.dp.sp.value, TextUnitType.Sp),
-                    color = MaterialTheme.colors.primaryVariant,
+                    fontSize = TextUnit(8F.dp.sp.value, TextUnitType.Sp),
+                    color = Color(0xFF7C7C7C),
                     text = exception
                 )
             }
