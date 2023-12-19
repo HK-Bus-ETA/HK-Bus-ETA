ce58e037870fd7fe7a96fc29086869f93d84d801
LOOHP <jamesloohp@gmail.com>
2023-12-19 15:14:29 +0000

    Missing plugin

--- HKBusETA/build.gradle.kts
+++ HKBusETA/build.gradle.kts
@@ -22,4 +22,5 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
