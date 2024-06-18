10e64430899b5a94209f15cb53db70354208ff15
LOOHP <jamesloohp@gmail.com>
2024-06-18 21:56:54 +0100

    No need to pump wearos version

--- HKBusETA/wearosApp/build.gradle.kts
+++ HKBusETA/wearosApp/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 30
         //noinspection OldTargetApi
         targetSdk = 33
-        versionCode = 680
+        versionCode = 678
         versionName = "2.1.3"
         vectorDrawables {
             useSupportLibrary = true
