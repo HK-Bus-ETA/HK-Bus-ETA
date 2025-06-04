fb872ce93e0c208d71641355e5d2ceba19ad6a04
LOOHP <jamesloohp@gmail.com>
2025-06-04 16:52:00 +0100

    Adapt to upstream TSW Circular fix

--- main.py
+++ main.py
@@ -441,25 +441,13 @@ def download_and_process_data_sheet():
     DATA_SHEET["routeList"]["AEL+1+Hong Kong+AsiaWorld-Expo"]["dest"]["zh"] = "機場及博覽館"
     DATA_SHEET["routeList"]["AEL+1+Hong Kong+AsiaWorld-Expo"]["dest"]["en"] = "Airport & AsiaWorld-Expo"
 
-    lrt_705_1_stops = DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Wing"]["stops"]["lightRail"]
-    lrt_705_2_stops = DATA_SHEET["routeList"]["705+1+Tin Wing+Tin Shui Wai"]["stops"]["lightRail"]
-    lrt_705_2_stops.pop(0)
-    DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai"] = DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Wing"]
-    DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai"]["dest"] = DATA_SHEET["routeList"]["705+1+Tin Wing+Tin Shui Wai"]["dest"]
-    DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai"]["stops"]["lightRail"] = lrt_705_1_stops + lrt_705_2_stops
-    DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai"]["lrtCircular"] = {"en": "TSW Circular", "zh": "天水圍循環綫"}
-    del DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Wing"]
-    del DATA_SHEET["routeList"]["705+1+Tin Wing+Tin Shui Wai"]
+    lrt_705_start = DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai (Circular)"]["stops"]["lightRail"][0]
+    DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai (Circular)"]["stops"]["lightRail"].append(lrt_705_start)
+    DATA_SHEET["routeList"]["705+1+Tin Shui Wai+Tin Shui Wai (Circular)"]["lrtCircular"] = {"en": "TSW Circular", "zh": "天水圍循環綫"}
 
-    lrt_706_1_stops = DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Wing"]["stops"]["lightRail"]
-    lrt_706_2_stops = DATA_SHEET["routeList"]["706+1+Tin Wing+Tin Shui Wai"]["stops"]["lightRail"]
-    lrt_706_2_stops.pop(0)
-    DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai"] = DATA_SHEET["routeList"]["706+1+Tin Wing+Tin Shui Wai"]
-    DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai"]["orig"] = DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Wing"]["orig"]
-    DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai"]["stops"]["lightRail"] = lrt_706_1_stops + lrt_706_2_stops
-    DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai"]["lrtCircular"] = {"en": "TSW Circular", "zh": "天水圍循環綫"}
-    del DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Wing"]
-    del DATA_SHEET["routeList"]["706+1+Tin Wing+Tin Shui Wai"]
+    lrt_706_start = DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai (Circular)"]["stops"]["lightRail"][0]
+    DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai (Circular)"]["stops"]["lightRail"].append(lrt_706_start)
+    DATA_SHEET["routeList"]["706+1+Tin Shui Wai+Tin Shui Wai (Circular)"]["lrtCircular"] = {"en": "TSW Circular", "zh": "天水圍循環綫"}
 
     kmb_ops = {}
     ctb_circular = {}
